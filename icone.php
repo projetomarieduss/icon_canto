e7b02dd01f322db3a34ae1320c86fe3e1c2b7be2
Maria Eduarda Santo Silva <79859389+projetomarieduss@users.noreply.github.com>
2022-08-20 12:04:13 -0300

    Add files via upload

---
+++ icone.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando</title>
+</head>
+<style>
+@media (max-width: 767px) {  
+    img{
+      margin-left: 70%;
+      margin-top: 145%;
+      position: fixed;  
+      color: blue;
+      width: 70px;
+     
+    }}
+
+@media (min-width: 280px) {  
+    img{
+      margin-left: 70%;
+      margin-top: 195%;
+      position: fixed;  
+      color: green;
+      width: 50px;
+     
+    }}
+    
+
+@media (min-width: 360px) {  
+    img{
+      margin-left: 70%;
+      margin-top: 175%;
+      position: fixed;  
+      color: green;
+      width: 50px;
+     
+    }}
+
+/*@media (min-width: 400px) {  
+    img{
+      margin-left: 70%;
+      margin-top: 15%;
+      position: fixed;  
+      color: green;
+      width: 50px;
+     
+    }}*/
+
+    
+@media (min-width: 390px) {  
+    img{
+      margin-left: 70%;
+      margin-top: 193%;
+      position: fixed;  
+      color: greenyellow;
+      width: 50px;
+     
+    }}
+
+
+@media (min-width: 540px) {  
+    img{
+      margin-left: 73%;
+      margin-top: 115%;
+      position: fixed;  
+      color: rgb(15, 77, 144);
+      width: 70px;
+     
+    }}
+
+
+@media (min-width: 768px) {  
+    img{
+      margin-left: 80%;
+      margin-top: 115%;
+      position: fixed;  
+      color: rgb(238, 0, 0);
+      width: 80px;
+     
+    }}
+
+@media (min-width: 820px) {  
+    img{
+      margin-left: 80%;
+      margin-top: 128%;
+      position: fixed;  
+      color: purple;
+      width: 90px;
+     
+    }}
+
+@media (min-width: 992px) {
+    img{
+      margin-left: 85%;
+      margin-top: 48%;
+      position: fixed;  
+      color: brown;
+      width: 90px;
+     
+    }
+}
+@media (min-width: 1200px) {
+    img{
+      margin-left: 90%;
+      margin-top: 42%;
+      position: fixed; 
+      color: rgb(7, 17, 45);  
+      width: 50px;
+     
+    }
+}
+@media (min-width: 1280px) {
+    img{
+      margin-left: 90%;
+      margin-top: 45%;
+      position: fixed;  
+      color: rgb(85, 85, 15);
+      width: 70px;
+     
+    }
+}
+  
+</style>
+<body>
+    <a href="index.php" target="_blank"><img src="img/icon.png" alt="Baixar curriculo" ></a>
+<div class="container">
+   <p>Este é o nosso teste principal para colocar o curriculo para correr</p>
+
+<?php
+$nome =  'Maria Eduarda Santo Silva'
+$experiencias = 
+?>
+</div>
+
+</body>
+</html>
